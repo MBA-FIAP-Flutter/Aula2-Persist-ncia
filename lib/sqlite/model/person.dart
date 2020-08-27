7878e9cdaa557b53d9049c84c53d4f3a6a45873d
Ricardo Ogliari <ricardo.ogliari@gok.digital>
2020-08-26 21:46:27 -0300

    adição da classe modelo de dados para Pessoa

---
+++ lib/sqlite/model/person.dart
@@ -0,0 +1,20 @@
+class Person {
+  int id;
+  final String firstName;
+  final String lastName;
+
+  Person({this.id, this.firstName, this.lastName});
+
+  //no Flutter.. vc faz parser de json-objetos e de objetos-json
+  //usando uma instância de Map
+  //dynamic: no DART, é uma tipagem fraca, que aceita qualquer coisa
+  //isso foi necessário, porque o pacote de sqlite trabalha com json na
+  //inserção e na leitura
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'firstName': firstName,
+      'lastName': lastName,
+    };
+  }
+}
