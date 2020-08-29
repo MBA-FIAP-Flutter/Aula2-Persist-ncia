3a41f0c0bcb53721184ae210213c308463ed43ab
Ricardo Ogliari <ricardo.ogliari@gok.digital>
2020-08-29 09:41:26 -0300

    tela de adição do livro

--- lib/nosql/database/app_database.dart
+++ lib/nosql/database/app_database.dart
@@ -4,7 +4,7 @@ import 'package:flutter_fiap_aula_2_bd/nosql/model/book.dart';
 import 'package:sqflite/sqflite.dart' as sqflite;
 import 'package:path/path.dart';
 
-part 'app_database.g.dart'; // the generated code will be there
+//part 'app_database.g.dart'; // the generated code will be there
 
 @Database(version: 1, entities: [Book])
 abstract class AppDatabase extends FloorDatabase {
