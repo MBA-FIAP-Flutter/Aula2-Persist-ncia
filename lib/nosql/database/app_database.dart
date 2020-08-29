9f2bffdfe933af76f5086ab6d236c18c16afafe2
Ricardo Ogliari <ricardo.ogliari@gok.digital>
2020-08-29 09:19:15 -0300

    código do database

---
+++ lib/nosql/database/app_database.dart
@@ -0,0 +1,12 @@
+import 'package:floor/floor.dart';
+import 'package:flutter_fiap_aula_2_bd/nosql/dao/book_dao.dart';
+import 'package:flutter_fiap_aula_2_bd/nosql/model/book.dart';
+import 'package:sqflite/sqflite.dart' as sqflite;
+import 'package:path/path.dart';
+
+part 'app_database.g.dart'; // the generated code will be there
+
+@Database(version: 1, entities: [Book])
+abstract class AppDatabase extends FloorDatabase {
+  BookDao get bookDao;
+}
