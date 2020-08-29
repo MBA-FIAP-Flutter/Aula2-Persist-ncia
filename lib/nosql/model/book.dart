2093bbb9cd537d237a5cec18d0dbf54c3784ec2d
Ricardo Ogliari <ricardo.ogliari@gok.digital>
2020-08-29 09:12:03 -0300

    criação da classe dao

--- lib/nosql/model/book.dart
+++ lib/nosql/model/book.dart
@@ -1,5 +1,6 @@
 import 'package:floor/floor.dart';
 
+//annotation entity indica pro flutter a presença de uma entidade
 @entity
 class Book {
   @PrimaryKey(autoGenerate: true)
@@ -8,5 +9,6 @@ class Book {
   final String name;
   final String author;
 
+  //construtor com parâmetros opcionais (uso das chaves)
   Book({this.id, this.name, this.author});
 }
